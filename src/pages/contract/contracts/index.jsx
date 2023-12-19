ffc28f4aaf42ab2602311bb14c737ee2c9724c23
Anh Khoa <anhkhoaphamnhat@gmail.com>
2023-12-19 14:55:58 +0700

    Finished initial page for contracts

--- src/pages/contract/contracts/index.jsx
+++ src/pages/contract/contracts/index.jsx
@@ -18,7 +18,8 @@ import { useForm } from 'react-hook-form';
 
 function Contracts() {
     return (
-        <><h3 className="text-2xl m-[20px] font-bold text-sky-600 uppercase text-center">Danh sách hợp đồng bảo hiểm</h3><div>
+        <>
+        <h3 className="text-2xl m-[20px] font-bold text-sky-600 uppercase text-center">Danh sách hợp đồng bảo hiểm</h3><div>
             <div className='grid grid-cols-2 gap-20 md-gap-40 m-12'>
                 <div className='col_span_1 flex items-center'>
                     <Label className="whitespace-nowrap m-2">Mã khách hàng:</Label>
@@ -49,11 +50,11 @@ function Contracts() {
                     
                     <TableHeader>
                         <TableRow>
-                        <TableHead>Số thứ tự</TableHead>
-                        <TableHead>Mã hợp đồng</TableHead>
-                        <TableHead>Tên người thụ hưởng</TableHead>
-                        <TableHead className="text-right">Trạng thái</TableHead>
-                        <TableHead className="text-right">Chi tiết thanh toán</TableHead>
+                        <TableHead >Số thứ tự</TableHead>
+                        <TableHead >Mã hợp đồng</TableHead>
+                        <TableHead >Tên người thụ hưởng</TableHead>
+                        <TableHead >Trạng thái</TableHead>
+                        <TableHead>Chi tiết thanh toán</TableHead>
                         </TableRow>
                     </TableHeader>
 
@@ -62,8 +63,8 @@ function Contracts() {
                         <TableCell>1</TableCell>
                         <TableCell>HD0001</TableCell>
                         <TableCell>Alex Nguyễn</TableCell>
-                        <TableCell className="text-right">Đang kích hoạt</TableCell>
-                        <TableCell className="text-right">
+                        <TableCell>Đang kích hoạt</TableCell>
+                        <TableCell>
                             <Button 
                                 style={{
                                     fontWeight: 'bold',
@@ -78,7 +79,8 @@ function Contracts() {
                 </Table>
 
             </div>
-        </div></>
+        </div>
+        </>
     );
 }
 
