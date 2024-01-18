ae5e45a0e26dd8f3f8219d2b397c4af76d6b102d
namkhanh1234 <namkhanh16052002@gmail.com>
2024-01-18 13:15:43 +0700

    feat-support

--- src/pages/payment/PaymentRequest/index.jsx
+++ src/pages/payment/PaymentRequest/index.jsx
@@ -96,71 +96,85 @@ function PaymentRequest() {
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center bg-sky-100 pb-10">
-                <h3 className="text-2xl m-[20px] font-bold text-sky-600 text-center">YÊU CẦU THANH TOÁN ĐIỀU TRỊ</h3>
-                <div className="flex flex-col items-center p-7 rounded-2xl border-2 bg-white">
-                    <div className="flex justify-evenly w-full">
-                        <div className="w-1/2 mr-2">
-                            <Label>Họ tên khách hàng</Label>
-                            <Input type="text" disabled defaultValue={user?.fullName} className="bg-slate-200"></Input>
+            <div className="flex justify-evenly  bg-sky-100">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center bg-sky-100 p-10">
+                    <h3 className="text-2xl m-2 font-bold text-sky-600 text-center">YÊU CẦU THANH TOÁN ĐIỀU TRỊ</h3>
+                    <div className="flex flex-col items-center p-5 rounded-2xl border-2 bg-white">
+                        <div className="flex justify-evenly w-full">
+                            <div className="w-1/2 mr-2">
+                                <Label>Họ tên khách hàng</Label>
+                                <Input
+                                    type="text"
+                                    disabled
+                                    defaultValue={user?.fullName}
+                                    className="bg-slate-200"
+                                ></Input>
+                            </div>
+                            <div className="w-1/2">
+                                <Label>Mã hợp đồng bảo hiểm</Label>
+                                <Input
+                                    type="text"
+                                    disabled
+                                    className="bg-slate-200"
+                                    defaultValue={contract?.insuranceCode}
+                                ></Input>
+                            </div>
                         </div>
-                        <div className="w-1/2">
-                            <Label>Mã hợp đồng bảo hiểm</Label>
-                            <Input
-                                type="text"
-                                disabled
-                                className="bg-slate-200"
-                                defaultValue={contract?.insuranceCode}
-                            ></Input>
+                        <div className="w-full mt-5">
+                            <Label htmlFor="message">Mô tả điều trị</Label>
+                            <Textarea placeholder="Nhập mô tả ở đây." id="message" {...register('description')} />
+                        </div>
+                        <div className="mt-5 flex w-full justify-evenly">
+                            <div className="w-1/2 mr-2">
+                                <Label htmlFor="picture">Ảnh hóa đơn hoặc hồ sơ bệnh án</Label>
+                                <Input
+                                    id="picture"
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={handleOnChange}
+                                    // {...register('photo')}
+                                />
+                            </div>
+                            <div className="w-1/2">
+                                <Label>Chi phí điều trị</Label>
+                                <Input
+                                    type="text"
+                                    placeholder="1000"
+                                    {...register('cost')}
+                                    value={costValue}
+                                    onChange={(e) => {
+                                        setValue('cost', e.target.value);
+                                        console.log(costValue);
+                                    }}
+                                    onBlur={(e) => {
+                                        setValue('cost', FormatCurrency(e.target.value));
+                                    }}
+                                ></Input>
+                            </div>
+                        </div>
+                        <div className="mt-4 flex justify-evenly w-full">
+                            <Button
+                                className="w-[140px] h-[42px] bg-sky-600 text-base rounded-md hover:bg-sky-700"
+                                type="submit"
+                            >
+                                Gửi
+                            </Button>
+                            <Button className="w-[140px] h-[42px] bg-red-600 text-base rounded-md hover:bg-red-700">
+                                Quay về
+                            </Button>
                         </div>
                     </div>
-
-                    <div className="w-full mt-5">
-                        <Label htmlFor="message">Mô tả điều trị</Label>
-                        <Textarea placeholder="Nhập mô tả ở đây." id="message" {...register('description')} />
-                    </div>
-                    <div className="mt-5 flex w-full justify-evenly">
-                        <div className="w-1/2 mr-2">
-                            <Label htmlFor="picture">Ảnh hóa đơn hoặc hồ sơ bệnh án</Label>
-                            <Input
-                                id="picture"
-                                type="file"
-                                accept="image/*"
-                                onChange={handleOnChange}
-                                // {...register('photo')}
-                            />
-                        </div>
-                        <div className="w-1/2">
-                            <Label>Chi phí điều trị</Label>
-                            <Input
-                                type="text"
-                                placeholder="1000"
-                                {...register('cost')}
-                                value={costValue}
-                                onChange={(e) => {
-                                    setValue('cost', e.target.value);
-                                    console.log(costValue);
-                                }}
-                                onBlur={(e) => {
-                                    setValue('cost', FormatCurrency(e.target.value));
-                                }}
-                            ></Input>
-                        </div>
-                    </div>
-                    {image && <img src={image.preview} className="w-full h-[30vh] mt-5"></img>}
-                    <div className="mt-4 flex justify-evenly w-full">
-                        <Button
-                            className="w-[140px] h-[42px] bg-sky-600 text-base rounded-md hover:bg-sky-700"
-                            type="submit"
-                        >
-                            Gửi
-                        </Button>
-                        <Button className="w-[140px] h-[42px] bg-red-600 text-base rounded-md hover:bg-red-700">
-                            Quay về
-                        </Button>
-                    </div>
+                </form>
+                <div>
+                    <p>Ảnh xem trước</p>
+                    {image && (
+                        <img
+                            src={image.preview}
+                            className="w-[50vw] h-[60vh] p-2 border-2 rounded m-1 border-gray-400"
+                        ></img>
+                    )}
                 </div>
-            </form>
+            </div>
         </>
     );
 }
